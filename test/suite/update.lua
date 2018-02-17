f2b54155569a4d97194ff69ba26787a66f90022c
David Corticchiato <david.corticchiato@gmail.com>
2018-02-17 15:39:50 +0100

    remove unused test

--- test/suite/update.lua
+++ test/suite/update.lua
@@ -8,27 +8,6 @@ local suite = {name = 'Update state', tests = {}}
 ----------------------------------------------
 
 function suite.tests.update1(it)
-  local test = it('should update offsets using velocity')
-
-  local function run(assert)
-    local offset = update.computeNewOffset(
-      {row = -1, col = 0},
-      1,
-      0.5
-    )
-
-    return assert.all(
-      assert.is(offset.row, -0.5),
-      assert.is(offset.col, 0)
-    )
-  end
-
-  return test, run
-end
-
-----------------------------------------------
-
-function suite.tests.update2(it)
   local test = it('should move to next tile')
 
   local function run(assert)
