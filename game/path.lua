133b8dcc86d800a3ea06fd826fd9634c86493d48
David Corticchiato <david.corticchiato@gmail.com>
2018-02-17 12:04:54 +0100

    findNextTile

--- game/path.lua
+++ game/path.lua
@@ -3,6 +3,13 @@ local path = {}
 path.WALL_TYPE = 'W'
 path.PATH_TYPE = '_'
 
+function path.isSameTile(tile1, tile2)
+  return tile1
+    and tile2
+    and (tile1.row == tile2.row)
+    and (tile1.col == tile2.col)
+end
+
 function path.findAdjacentTiles(tile, map)
   local adjacentTiles = {}
   local tileType = map.tiles[tile.row][tile.col]
@@ -23,8 +30,27 @@ function path.findAdjacentTiles(tile, map)
 end
 
 function path.findNextTile(actor, map)
+  local nextTile = actor.tile
+  local adjacentTiles = path.findAdjacentTiles(actor.tile, map)
 
+  for __, adjacentTile in pairs(adjacentTiles) do
+    local beenThere = false
 
+    for __, seenTile in pairs(actor.seenTiles) do
+      beenThere = path.isSameTile(adjacentTile, seenTile)
+
+      if beenThere then
+        break
+      end
+    end
+
+    if not beenThere then
+      nextTile = adjacentTile
+      break
+    end
+  end
+
+  return nextTile
 end
 
 return path
--- test/suite/path.lua
+++ test/suite/path.lua
@@ -27,4 +27,49 @@ end
 
 ----------------------------------------------
 
+function suite.tests.path2(it)
+  local test = it('should find next tile in path')
+
+  local function run(assert)
+    local nextTile = path.findNextTile(
+      {tile = {row = 1, col = 3}, seenTiles = {}},
+      map1Fixture
+    )
+
+    return assert.all(
+      assert.is(nextTile.row, 2),
+      assert.is(nextTile.col, 3)
+    )
+  end
+
+  return test, run
+end
+
+----------------------------------------------
+
+function suite.tests.path3(it)
+  local test = it('should find only unseen tile in path')
+
+  local function run(assert)
+    local nextTile = path.findNextTile(
+      {
+        tile = {row = 2, col = 3},
+        seenTiles = {
+          {row = 1, col = 3}
+        }
+      },
+      map1Fixture
+    )
+
+    return assert.all(
+      assert.is(nextTile.row, 2),
+      assert.is(nextTile.col, 4)
+    )
+  end
+
+  return test, run
+end
+
+----------------------------------------------
+
 return suite
