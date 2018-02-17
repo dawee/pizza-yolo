a64888fbc7a1f1c5567390554d78a4f298f742b8
David Corticchiato <david.corticchiato@gmail.com>
2018-02-17 13:14:11 +0100

    drawAt

--- game/draw.lua
+++ game/draw.lua
@@ -4,7 +4,17 @@ local draw = {}
 
 draw.SCALE = 8
 draw.SIZE = 16
-draw.SCALED_SIZE = draw.SIZE * draw.SCALE / 2
+
+local function drawAt(image, row, col, size)
+  love.graphics.draw(
+    image,
+    size * draw.SIZE * draw.SCALE * (col - 1) / 2,
+    size * draw.SIZE * draw.SCALE * (row - 1) / 2,
+    0,
+    draw.SCALE,
+    draw.SCALE
+  )
+end
 
 function draw.tiles(graphics, state)
   for row, tilesRow in ipairs(state.map.tiles) do
@@ -13,14 +23,7 @@ function draw.tiles(graphics, state)
         and graphics.images.tilePath
         or graphics.images.tileWall
 
-      love.graphics.draw(
-        image,
-        draw.SCALED_SIZE * (col - 1),
-        draw.SCALED_SIZE * (row - 1),
-        0,
-        draw.SCALE,
-        draw.SCALE
-      )
+        drawAt(image, row, col, 1)
     end
   end
 end
