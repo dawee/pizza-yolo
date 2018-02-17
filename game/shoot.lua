e435a70a5e3ffa5b93636a16c432aa3c3f65d37c
Estelle Martinez <estelle.eliakon@gmail.com>
2018-02-17 18:32:42 +0100

    Find mobs near tower

--- game/shoot.lua
+++ game/shoot.lua
@@ -2,8 +2,17 @@ local shoot = {}
 
 function shoot.findMobsNearTower(tower, state)
   -- tower.radius
+  local found = {}
+  for _, mob in pairs(state.mobs) do
+    local distanceRow = math.abs(tower.tile.row - mob.tile.row)
+    local distanceCol = math.abs(tower.tile.col - mob.tile.col)
+    if distanceCol + distanceRow <= tower.radius then
+      table.insert(found, mob.id)
+    end
+  end
+  print(found)
 
-  return {1, 2} -- returns a list of mob ids
+  return found -- returns a list of mob ids
 end
 
 return shoot
