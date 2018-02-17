82cbf939d158f180cb7718d48434a6913cae301a
David Corticchiato <david.corticchiato@gmail.com>
2018-02-17 18:51:05 +0100

    lint

--- game/shoot.lua
+++ game/shoot.lua
@@ -1,8 +1,8 @@
 local shoot = {}
 
 function shoot.findMobsNearTower(tower, state)
-  -- tower.radius
   local found = {}
+
   for _, mob in pairs(state.mobs) do
     local distanceRow = math.abs(tower.tile.row - mob.tile.row)
     local distanceCol = math.abs(tower.tile.col - mob.tile.col)
@@ -10,9 +10,8 @@ function shoot.findMobsNearTower(tower, state)
       table.insert(found, mob.id)
     end
   end
-  print(found)
 
-  return found -- returns a list of mob ids
+  return found
 end
 
 return shoot
