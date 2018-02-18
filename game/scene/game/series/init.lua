2c49bc7e8b7049d18f217868c6efdfb15ff0fc30
David Corticchiato <david.corticchiato@gmail.com>
2018-02-18 14:47:58 +0100

    cheddar series

--- game/scene/game/series/init.lua
+++ game/scene/game/series/init.lua
@@ -3,7 +3,7 @@
 local function mozza()
   return {
     type = 'mozza',
-    lives = 8,
+    lives = 4,
     velocity = 0.8,
   }
 end
@@ -11,7 +11,7 @@ end
 local function cheddar()
   return {
     type = 'cheddar',
-    lives = 32,
+    lives = 70,
     velocity = 0.8,
   }
 end
@@ -38,13 +38,28 @@ local function series5()
   return {mozza(), cheddar(), mozza()}
 end
 
+local function series6()
+  return {mozza(), cheddar(), mozza()}
+end
+
+local function series7()
+  return {cheddar(), cheddar()}
+end
+
+local function series8()
+  return {cheddar(), cheddar()}
+end
+
 local function createSeries()
   return {
     series1(),
     series2(),
     series3(),
     series4(),
-    series5()
+    series5(),
+    series6(),
+    series7(),
+    series8()
   }
 end
 
